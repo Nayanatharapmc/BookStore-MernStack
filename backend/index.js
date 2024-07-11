0a510c6cc7b80ab7e9d7912c8a1194843873e609
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2024-07-11 23:37:48 +0530

    CORS policy implemented

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import {Book} from "./models/BookModel.js";
 import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 //import dotenv from "dotenv";
 //dotenv.config();
 
@@ -11,6 +12,16 @@ const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for enabling CORS policy
+// Option1: Allow all origins with default of cors(*)
+//app.use(cors());
+// Option2: Allow custom origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 
 app.get('/',(request,response) => {
     console.log(request);
